1481113b4f5ea1e8c77c45b9b7a837f85ed960b4
Kerven Kildhery <kervensilvva@gmail.com>
2025-01-04 21:48:50 -0300

    configuradas rotas para login, registro e lista de tarefas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: CadastroComponent },
+    { path: 'tasks', component: TaskListComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule {}
